71ec190308df1b5770c20e9130444de0ea37bb1f
Tolga Bağcı <116670273+tolgabagcii@users.noreply.github.com>
2025-08-12 21:45:50 +0300

    Add files via upload

---
+++ homepage/assets/sections/sections.js
@@ -0,0 +1,157 @@
+const coursesData = [
+    {
+        "baslik": "Goethe A1",
+        "aciklama": "Temel konular ve başlangıç seviyesi alıştırmalar.",
+        "gorselUrl": "",
+        "detayliAciklama": "",
+        "kategori": "GOETHE A1",
+        "altBolumler": [
+            {
+                "baslik": "Konu Anlatımı",
+                "aciklama": "Temel seviyeye uygun gramer konuları, kelime listeleri ve örnek cümlelerle birlikte ayrıntılı olarak anlatılır.",
+                "gorselUrl": "",
+                "detayliAciklama": "Temel seviyeye uygun gramer konuları, kelime listeleri ve örnek cümlelerle birlikte ayrıntılı olarak anlatılır.",
+                "kategori": "GOETHE A1",
+                "altBolumler": [
+                        {
+                            "baslik": "1. MODÜL",
+                            "aciklama": "Almanca öğrenmeye yeni başlayanlar için harfler, fiiller, tanışma cümleleri ve günlük ifadeleri içeren temel konuların anlatımı ve uygulamaları.",
+                            "gorselUrl": "",
+                            "detayliAciklama": "",
+                            "kategori": "GOETHE A1",
+                            "altBolumler": [
+                                {
+                                    "baslik": "1) Harfler ve Okunuşları",
+                                    "aciklama": "Almanca alfabedeki harfleri ve bu harflerin doğru telaffuzlarını öğrenin. Okuma ve konuşma becerilerinizin temelini oluşturur.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Almanca alfabedeki harfleri ve bu harflerin doğru telaffuzlarını öğrenin. Okuma ve konuşma becerilerinizin temelini oluşturur.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "2) Fiil Çekimleri",
+                                    "aciklama": "Almanca’da fiillerin öznelere göre nasıl çekimlendiğini öğrenin. Doğru cümle kurabilmek için temel dil bilgisi yapılarından biridir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Almanca’da fiillerin öznelere göre nasıl çekimlendiğini öğrenin. Doğru cümle kurabilmek için temel dil bilgisi yapılarından biridir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "3) Fiiller ve Cevapları",
+                                    "aciklama": "Günlük hayatta sık kullanılan fiillerle sorulara nasıl yanıt verileceğini öğrenin. Sözlü iletişiminizi güçlendirir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Günlük hayatta sık kullanılan fiillerle sorulara nasıl yanıt verileceğini öğrenin. Sözlü iletişiminizi güçlendirir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "4) Kendini tanıtma",
+                                    "aciklama": "Kişisel bilgilerinizi Almanca olarak ifade etmeyi öğrenin. Tanışma ve sosyal etkileşimlerde ilk adımdır.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Kişisel bilgilerinizi Almanca olarak ifade etmeyi öğrenin. Tanışma ve sosyal etkileşimlerde ilk adımdır.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "5) Vurgulu Cümle",
+                                    "aciklama": "Cümlede vurguyu nasıl yapacağınızı öğrenin. Almanca’da anlamı güçlendiren yapılarla iletişiminizi daha etkili hale getirin.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Cümlede vurguyu nasıl yapacağınızı öğrenin. Almanca’da anlamı güçlendiren yapılarla iletişiminizi daha etkili hale getirin.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "6) Günler",
+                                    "aciklama": "Almanca’da haftanın günlerini öğrenin. Tarih belirtme, plan yapma ve günlük konuşmalar için temel bilgidir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Almanca’da haftanın günlerini öğrenin. Tarih belirtme, plan yapma ve günlük konuşmalar için temel bilgidir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                }
+                            ]
+                            
+                        },
+                        {
+                            "baslik": "2. MODÜL",
+                            "aciklama": "Bu bölümde, Almanca’da cümle kurulumunun temelini oluşturan dil bilgisi konularını öğrenebilirsiniz. Artikellerden yardımcı fiillere kadar önemli yapılar sade ve anlaşılır şekilde sunulmuştur.",
+                            "gorselUrl": "",
+                            "detayliAciklama": "",
+                            "kategori": "GOETHE A1",
+                            "altBolumler": [
+                                {
+                                    "baslik": "1) Artikeller Konu Anlatımı",
+                                    "aciklama": "Almanca'da isimlerin başında yer alan belirli ve belirsiz artikellerin kullanımını öğrenin. Cümle kurarken doğru artikel seçimi dilin temelidir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Almanca'da isimlerin başında yer alan belirli ve belirsiz artikellerin kullanımını öğrenin. Cümle kurarken doğru artikel seçimi dilin temelidir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "2) Zamirler",
+                                    "aciklama": "Kişi, iyelik, işaret ve diğer zamir türlerinin Almanca’daki kullanım şekillerini öğrenin. Cümlelerde özne ve nesneleri doğru ifade etmenizi sağlar.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Kişi, iyelik, işaret ve diğer zamir türlerinin Almanca’daki kullanım şekillerini öğrenin. Cümlelerde özne ve nesneleri doğru ifade etmenizi sağlar.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "3) Sıfat Tamlamaları",
+                                    "aciklama": "İsimleri niteleyen sıfatların, artikellere ve ismin durumuna göre nasıl çekimlendiğini öğrenin. Doğru ve etkili tanımlamalar yapabilmek için önemlidir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "İsimleri niteleyen sıfatların, artikellere ve ismin durumuna göre nasıl çekimlendiğini öğrenin. Doğru ve etkili tanımlamalar yapabilmek için önemlidir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "4) Edatlar",
+                                    "aciklama": "Yer, zaman ve yön bildiren temel edatların kullanımını öğrenin. Cümledeki anlam ilişkilerini doğru kurmak için gereklidir.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Yer, zaman ve yön bildiren temel edatların kullanımını öğrenin. Cümledeki anlam ilişkilerini doğru kurmak için gereklidir.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                },
+                                {
+                                    "baslik": "5) Yardımcı Fiiller",
+                                    "aciklama": "Almanca’da geniş zaman, geçmiş zaman ve kip yapılarında kullanılan temel yardımcı fiilleri öğrenin. Zaman çekimlerinin doğru kurulmasını sağlar.",
+                                    "gorselUrl": "",
+                                    "detayliAciklama": "Almanca’da geniş zaman, geçmiş zaman ve kip yapılarında kullanılan temel yardımcı fiilleri öğrenin. Zaman çekimlerinin doğru kurulmasını sağlar.",
+                                    "kategori": "Konu Anlatım",
+                                    "quizUrl": "/",
+                                    "jsonDosyalari": [""]
+                                }
+                            ]
+                    }
+                ]
+                
+            },
+            {
+                "baslik": "Alıştırmalar",
+                "aciklama": "Bu bölümde, Almanca A1 seviyesine uygun dil bilgisi ve kelime bilgisi konularını kapsayan Alıştırmalarla bilginizi ölçebilir, öğrendiklerinizi pekiştirebilirsiniz.",
+                "gorselUrl": "",
+                "detayliAciklama": "",
+                "kategori": "GOETHE A1",
+                "altBolumler": [
+                    {
+                        "baslik": "Goethe Fiiller Listesi",
+                        "aciklama": "Goethe Enstitüsü’nün belirlediği temel fiiller listesine dayalı bu bölümde, sık kullanılan fiillerin anlamlarını, çekimlerini ve doğru kullanımlarını pekiştirebilirsiniz.",
+                        "gorselUrl": "",
+                        "detayliAciklama": "Goethe Enstitüsü’nün temel fiiller listesine dayanan bu bölüm, sık kullanılan fiillerin anlamlarını, çekimlerini ve doğru kullanımlarını öğrenip pekiştirmeni sağlar.",
+                        "kategori": "Alıştırma",
+                        "quizUrl": "sections/index.html",
+                        "jsonDosyalari": ["json1.js", "json2.js"]             
+                    },                    
+                ]
+            }
+        ]
+    },
+];
